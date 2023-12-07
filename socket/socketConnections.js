90e698bd9c77cbc77b5a53d49600523abb9ac7ac
princewill-broadshift <princewill.d@broadshift.com>
2023-12-07 06:23:16 +0100

    added crypto-js

--- socket/socketConnections.js
+++ socket/socketConnections.js
@@ -6,7 +6,7 @@ import {
   saveMessageToDB,
 } from "../controllers/MessagesController.js";
 import Users from "../models/Users.js";
-// import { encryptMessage } from "../utils/helpers.js";
+import { encryptMessage } from "../utils/helpers.js";
 import { sendPushNotification } from "../utils/pushNotification.js";
 
 const configureSocketIO = (httpServer) => {
@@ -54,7 +54,7 @@ const configureSocketIO = (httpServer) => {
             receiverSocketId = receiver?.socketId;
 
             // Encrypt the message
-            // const encryptedMessage = encryptMessage(message);
+            const encryptedMessage = encryptMessage(message);
 
             // for push notification
             await sendPushNotification({
@@ -69,7 +69,7 @@ const configureSocketIO = (httpServer) => {
               receiverId,
               createdAt,
               senderPhoneNumber,
-              message,
+              message: encryptedMessage,
               isMessageReceived: true,
               isMessageSent: false,
               senderId,
@@ -89,7 +89,7 @@ const configureSocketIO = (httpServer) => {
                 receiverId,
                 senderId,
                 createdAt,
-                message,
+                message: encryptedMessage,
                 isMessageReceived: true,
                 isMessageSent: false,
                 isRead,
@@ -128,13 +128,13 @@ const configureSocketIO = (httpServer) => {
           const senderSocketId = sender?.socketId;
 
           // Encrypt the message
-          // const encryptedMessage = encryptMessage(message);
+          const encryptedMessage = encryptMessage(message);
 
           await saveMessageToDB({
             receiverId,
             createdAt,
             senderPhoneNumber,
-            message,
+            message: encryptedMessage,
             isMessageReceived: false,
             isMessageSent: true,
             senderId,
@@ -151,7 +151,7 @@ const configureSocketIO = (httpServer) => {
             receiverId,
             senderId,
             createdAt,
-            message,
+            message: encryptedMessage,
             isMessageReceived: false,
             isMessageSent: true,
             online: receiver?.online,
