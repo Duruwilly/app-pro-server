990991bf635be202a939879fd7d94f4cb3b054c4
princewill-broadshift <princewill.d@broadshift.com>
2023-12-07 13:54:36 +0100

    hardcoded push token

--- utils/helpers.js
+++ utils/helpers.js
@@ -68,6 +68,7 @@ export const sendEmail = async (options) => {
 };
 
 export const addPushToken = async (userId, pushToken) => {
+  console.log("pushToken", userId, pushToken);
   try {
     const user = await Users.findById(userId);
     if (user) {
