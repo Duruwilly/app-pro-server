d439cf445007f66a6551f52fbaf34d393efa4158
princewill-broadshift <princewill.d@broadshift.com>
2024-01-01 12:10:05 +0100

    updates ft: added secure to true for cloudinary

--- utils/cloudinaryConfig.js
+++ utils/cloudinaryConfig.js
@@ -8,6 +8,7 @@ cloudinaryV2.config({
   cloud_name: process.env.CLOUDINARY_NAME,
   api_key: process.env.CLOUDINARY_KEY,
   api_secret: process.env.CLOUDINARY_SECRET,
+  secure: true,
 });
 
 export const storage = new CloudinaryStorage({
